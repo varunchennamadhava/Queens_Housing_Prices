a7cff4b2710467440f36beb22e0b26efc252f46a
Varun Chennamadhava <varunchennamadhava@mac.lan>
2025-04-26 22:01:38 -0400

    added a bunch of code to make things work

--- Queens.R
+++ Queens.R
@@ -21,7 +21,8 @@ Queens2023_Filtered <- subset(
     "YEAR.BUILT",
     "SALE.PRICE",
     "LAND..SQUARE.FEET",
-    "GROSS..SQUARE.FEET"
+    "GROSS..SQUARE.FEET",
+    "BUILDING.CLASS.AT.TIME.OF.SALE"
   )
 )
 
@@ -30,3 +31,49 @@ head(Queens2023_Filtered)
 
 # Alternatively, both dimensions (rows, cols)
 dim(Queens2023_Filtered)   # first element is rows, second is columns
+
+
+# Alternatively, using na.omit()
+Queens2023_Clean <- na.omit(Queens2023_Filtered)
+Queens2023_Clean <- subset(Queens2023_Clean, SALE.PRICE != 0)
+Queens2023_Clean$SALE.PRICE <- as.numeric(
+  gsub(",", "", Queens2023_Clean$SALE.PRICE)
+)
+Queens2023_Clean$GROSS..SQUARE.FEET <- as.numeric(
+  gsub(",", "", as.character(Queens2023_Clean$GROSS..SQUARE.FEET))
+)
+Queens2023_Clean$LAND..SQUARE.FEET <- as.numeric(
+  gsub(",", "", as.character(Queens2023_Clean$LAND..SQUARE.FEET))
+)
+
+
+
+
+# Check that all rows are now complete
+nrow(Queens2023_Clean)
+head(Queens2023_Filtered)
+head(Queens2023_Clean)
+colnames(Queens2023_Clean)
+
+
+
+Queens2023_Model <- lm(
+    SALE.PRICE ~
+    TOTAL..UNITS +                       # quantitative
+    YEAR.BUILT +                          # quantitative
+    LAND..SQUARE.FEET +                  # quantitative
+    GROSS..SQUARE.FEET,                  # quantitative
+  data = Queens2023_Clean
+)
+
+
+# View the coefficient estimates and model summary
+#summary(Queens2023_Model)
+
+#formula(Queens2023_Model)
+
+coef(Queens2023_Model)
+
+
+#class(Queens2023_Clean$GROSS..SQUARE.FEET)    
+#class(Queens2023_Clean$LAND..SQUARE.FEET)   
